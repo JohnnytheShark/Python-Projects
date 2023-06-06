b0317f373d2888f04746e07c958c2656d37b25f8
Jonathan Orellana <Jonathan.Orellana2@nscorp.com>
2023-06-06 10:13:37 -0400

    Update FormattingColumns.py

--- FormattingColumns.py
+++ FormattingColumns.py
@@ -6,3 +6,6 @@ df['column'] = df['column'].astype(float).apply(lambda x: "${:,.2f}".format(x))
 
 #Mulitple columns selected: 
 df.iloc[:,3:] = df.iloc[:,3:].astype(float).applymap(lambda x: "${:,.2f}".format(x))
+
+#Formatting multiple columns to floats
+data[data.columns[3:]] = data.iloc[:,3:].astype(str).replace(r'[$,\s]',"",regex=True).applymap(lambda x: pd.to_numeric(x))
